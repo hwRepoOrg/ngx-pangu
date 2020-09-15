354c3da908eb445d6a20ed6295a70e1ba35c1e12
MrHuangJser <565628147@qq.com>
2020-09-15 17:06:50 +0800

    MrHuangJser - improve toolbar

--- src/app/components/toolbar/toolbar.component.ts
+++ src/app/components/toolbar/toolbar.component.ts
@@ -1,7 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy } from '@angular/core';
 import { Store } from '@ngrx/store';
-import { Observable } from 'rxjs';
+import { Observable, Subscription } from 'rxjs';
 import { map } from 'rxjs/operators';
+import { scaleCanvas } from 'src/app/store/actions';
 import { IStore } from 'src/app/store/store';
 
 @Component({
@@ -9,8 +10,9 @@ import { IStore } from 'src/app/store/store';
   templateUrl: './toolbar.component.html',
   styleUrls: ['./toolbar.component.less'],
 })
-export class ToolbarComponent implements OnInit {
+export class ToolbarComponent implements OnDestroy {
   public selected$: Observable<Set<string>>;
+  public scale: number;
   public get copyStatus$(): Observable<boolean> {
     return this.selected$.pipe(map((state) => state.size === 0));
   }
@@ -20,9 +22,22 @@ export class ToolbarComponent implements OnInit {
   public get breakStatus$(): Observable<boolean> {
     return this.selected$.pipe(map((state) => state.size > 1 || state.size === 0));
   }
+  private subscription = new Subscription();
+
   constructor(private store: Store<IStore>) {
     this.selected$ = this.store.select('canvasState').pipe(map((state) => state.selected));
+    this.subscription.add(
+      this.store.select('canvasState').subscribe((state) => {
+        this.scale = state.scale;
+      })
+    );
   }
 
-  ngOnInit(): void {}
+  ngOnDestroy(): void {
+    this.subscription.unsubscribe();
+  }
+
+  setCanvasScale(scale: number): void {
+    this.store.dispatch(scaleCanvas({ scale: scale / 100 }));
+  }
 }
