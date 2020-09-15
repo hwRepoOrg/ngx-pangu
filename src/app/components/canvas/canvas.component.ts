1509567f8417cc38ab6b63b52c7e6c72c78f8a5c
MrHuangJser <565628147@qq.com>
2020-09-15 17:31:24 +0800

    MrHuangJser - add canvas component

---
+++ src/app/components/canvas/canvas.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'ce-canvas',
+  templateUrl: './canvas.component.html',
+  styleUrls: ['./canvas.component.less']
+})
+export class CanvasComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
