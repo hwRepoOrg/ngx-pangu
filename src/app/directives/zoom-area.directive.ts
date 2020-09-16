21e7d9f3076e337f31e6467eba85c33e6f2405ca
MrHuangJser <565628147@qq.com>
2020-09-16 10:55:22 +0800

    MrHuangJser - improve main page sytle

--- src/app/directives/zoom-area.directive.ts
+++ src/app/directives/zoom-area.directive.ts
@@ -1,7 +1,6 @@
 import { Directive, HostBinding } from '@angular/core';
 import { Store } from '@ngrx/store';
 import { Subscription } from 'rxjs';
-import { filter, map } from 'rxjs/operators';
 import { IStore } from '../store/store';
 
 @Directive({
@@ -12,17 +11,18 @@ export class ZoomAreaDirective {
   transformOrigin = '0 0';
   @HostBinding('style.transform')
   public scale: string;
+  @HostBinding('style.width.px')
+  width: number;
+  @HostBinding('style.height.px')
+  height: number;
   private subscription = new Subscription();
   constructor(private store: Store<IStore>) {
     this.subscription.add(
-      this.store
-        .select('canvasState')
-        .pipe(
-          map((state) => state.scale),
-          filter((scale) => !!scale),
-          map((scale) => `scale(${scale})`)
-        )
-        .subscribe((scale) => (this.scale = scale))
+      this.store.select('canvasState').subscribe((state) => {
+        this.scale = `scale(${state.scale})`;
+        this.width = state.width;
+        this.height = state.height;
+      })
     );
   }
 }
