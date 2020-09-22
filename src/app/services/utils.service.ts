a550f57fb0a473f905711501df40cb2f5540a31f
MrHuangJser <565628147@qq.com>
2020-09-22 17:03:58 +0800

    MrHuangJser - add util func

--- src/app/services/utils.service.ts
+++ src/app/services/utils.service.ts
@@ -1,6 +1,14 @@
 import { Injectable } from '@angular/core';
 import { INode } from '../store/store';
 
+type IPosition = [number, number];
+interface IAbsolutePosition {
+  tl: IPosition;
+  tr: IPosition;
+  bl: IPosition;
+  br: IPosition;
+}
+
 @Injectable({ providedIn: 'root' })
 export class CeUtilsService {
   constructor() {}
@@ -21,5 +29,30 @@ export class CeUtilsService {
       }
     }
     return node;
+  }
+
+  public getAbsolutePosition(cx: number, cy: number, width: number, height: number, rotate: number): IAbsolutePosition {
+    switch (rotate) {
+      case 0:
+      case 360:
+        return {
+          tl: [cx - width / 2, cy - height / 2],
+          tr: [cx + width / 2, cy - height / 2],
+          bl: [cx - width / 2, cy + height / 2],
+          br: [cx + width / 2, cy + height / 2],
+        };
+      case 90:
+        return {
+          tl: [cx - width / 2, cy - height / 2],
+          tr: [cx + width / 2, cy - height / 2],
+          bl: [cx - width / 2, cy + height / 2],
+          br: [cx + width / 2, cy + height / 2],
+        };
+        break;
+      case 180:
+        break;
+      case 270:
+        break;
+    }
   }
 }
