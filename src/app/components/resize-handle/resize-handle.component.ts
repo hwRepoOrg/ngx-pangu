2f6b6c340c7b5d3cfd7a0a87640711b717f6d1ba
MrHuangJser <565628147@qq.com>
2020-09-25 17:40:17 +0800

    MrHuangJser - add rotate action

--- src/app/components/resize-handle/resize-handle.component.ts
+++ src/app/components/resize-handle/resize-handle.component.ts
@@ -1,9 +1,11 @@
-import { Component, HostBinding, OnDestroy } from '@angular/core';
+import { Component, ElementRef, HostBinding, OnDestroy } from '@angular/core';
 import { Store } from '@ngrx/store';
+import { evaluate } from 'mathjs';
 import { Subscription } from 'rxjs';
 import { filter, map } from 'rxjs/operators';
 import { CeUtilsService } from 'src/app/services/utils.service';
-import { ICanvasPosition, ICanvasSize, INode, IStore } from 'src/app/store/store';
+import { updateNodes } from 'src/app/store/actions';
+import { ICanvasPosition, INode, IStore } from 'src/app/store/store';
 
 @Component({
   selector: 'ce-resize-handle',
@@ -24,18 +26,17 @@ export class ResizeHandleComponent implements OnDestroy {
   @HostBinding('style.transform')
   public transform: string;
   private subscription = new Subscription();
-  private selected: Set<string>;
-  private canvasSize: ICanvasSize;
+  public selected: Set<string>;
   private canvasPosition: ICanvasPosition;
   private nodes: INode[];
-  constructor(private store: Store<IStore>, private utils: CeUtilsService) {
+  public rotateSnapshot: [number, number, number, number, INode] = null;
+  constructor(private store: Store<IStore>, private utils: CeUtilsService, private eleRef: ElementRef<HTMLElement>) {
     this.subscription.add(
       this.store.select('canvasPosition').subscribe((state) => {
         this.canvasPosition = state;
         this.refreshResizeHandleBoundingBox();
       })
     );
-    this.subscription.add(this.store.select('canvasSize').subscribe((state) => (this.canvasSize = state)));
     this.subscription.add(
       this.store.select('nodes').subscribe((nodes) => {
         this.nodes = nodes;
@@ -114,4 +115,59 @@ export class ResizeHandleComponent implements OnDestroy {
       this.transform = `rotate(${rotate ?? 0}deg)`;
     });
   }
+
+  rotateStart(event: PointerEvent): void {
+    event.stopPropagation();
+    event.preventDefault();
+    const rect = this.eleRef.nativeElement.getBoundingClientRect().toJSON();
+    this.rotateSnapshot = [
+      rect.left + rect.width / 2,
+      rect.top + rect.height / 2,
+      event.clientX,
+      event.clientY,
+      this.utils.getNodeById([...this.selected][0], this.nodes),
+    ];
+  }
+
+  rotating(event: PointerEvent): void {
+    if (this.rotateSnapshot) {
+      const [cx, cy, sx, sy, node] = this.rotateSnapshot;
+      const ex = event.clientX;
+      const ey = event.clientY;
+      const moveRotate = getRotate(cx, cy, sx, sy, ex, ey, node.rotate ?? 0);
+      const patchedRotate = (node.rotate ?? 0) + Math.round(moveRotate);
+      const relativeRotate = patchedRotate >= 360 ? patchedRotate % 360 : patchedRotate;
+      const absoluteRotate = relativeRotate < 0 ? 360 + relativeRotate : relativeRotate;
+      this.store.dispatch(
+        updateNodes({
+          nodes: [{ ...node, rotate: absoluteRotate }],
+        })
+      );
+    }
+  }
+
+  rotateStop(): void {
+    this.rotateSnapshot = null;
+  }
+}
+
+function getRotate(cx: number, cy: number, sx: number, sy: number, ex: number, ey: number, originalRotate: number): number {
+  const rotate = evaluate(
+    `acos(((cx-sx)^2+(cy-sy)^2+(cx-ex)^2+(cy-ey)^2 - (sx-ex)^2-(sy-ey)^2)/(2*sqrt((cx-sx)^2+(cy-sy)^2)*sqrt((cx-ex)^2+(cy-ey)^2)))*180/PI`,
+    {
+      cx,
+      cy,
+      ex,
+      ey,
+      sx,
+      sy,
+    }
+  );
+  const y = getLineYAxisByXAxis(ex, cx, cy, sx, sy);
+
+  return ey > y ? (originalRotate > 180 ? -rotate : rotate) : originalRotate > 180 ? rotate : -rotate;
+}
+
+function getLineYAxisByXAxis(x: number, cx: number, cy: number, sx: number, sy: number): number {
+  return evaluate(`(x - cx)*(sy-cy)/(sx-cx)+cy`, { x, cx, cy, sx, sy });
 }
