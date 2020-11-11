43045bb0535c0e9feac9050ece348eddced39920
MrHuangJser <565628147@qq.com>
2020-11-11 16:58:27 +0800

    MrHuangJser - add groupNodes method

--- src/app/components/toolbar/toolbar.component.ts
+++ src/app/components/toolbar/toolbar.component.ts
@@ -2,7 +2,7 @@ import { Component, OnDestroy } from '@angular/core';
 import { Store } from '@ngrx/store';
 import { Observable, Subscription } from 'rxjs';
 import { map } from 'rxjs/operators';
-import { updateCanvasPosition } from 'src/app/store/actions';
+import { clearBorderedNodes, clearSelectedNodes, groupNodes, updateCanvasPosition } from 'src/app/store/actions';
 import { IStore } from 'src/app/store/store';
 
 @Component({
@@ -22,15 +22,13 @@ export class ToolbarComponent implements OnDestroy {
   public get breakStatus$(): Observable<boolean> {
     return this.selected$.pipe(map((state) => state.size > 1 || state.size === 0));
   }
+  private selected: Set<string>;
   private subscription = new Subscription();
 
   constructor(private store: Store<IStore>) {
     this.selected$ = this.store.select('selected');
-    this.subscription.add(
-      this.store.select('canvasPosition').subscribe((state) => {
-        this.scale = state.scale;
-      })
-    );
+    this.subscription.add(this.store.select('canvasPosition').subscribe((state) => (this.scale = state.scale)));
+    this.subscription.add(this.selected$.subscribe((selected) => (this.selected = selected)));
   }
 
   ngOnDestroy(): void {
@@ -39,5 +37,11 @@ export class ToolbarComponent implements OnDestroy {
 
   setCanvasScale(scale: number): void {
     this.store.dispatch(updateCanvasPosition({ scale: scale / 100 }));
+  }
+
+  groupNodes(): void {
+    this.store.dispatch(groupNodes({ ids: [...this.selected] }));
+    this.store.dispatch(clearSelectedNodes());
+    this.store.dispatch(clearBorderedNodes());
   }
 }
