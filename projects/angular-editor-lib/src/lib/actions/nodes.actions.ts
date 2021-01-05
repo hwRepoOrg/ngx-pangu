23c355727cef9ac50227c0e3d694ef05d2d9f6e6
MrHuangJser <565628147@qq.com>
2021-01-05 17:07:26 +0800

    MrHuangJser - modify resizing calculation

--- projects/angular-editor-lib/src/lib/actions/nodes.actions.ts
+++ projects/angular-editor-lib/src/lib/actions/nodes.actions.ts
@@ -178,33 +178,6 @@ export function breakNode<T = any>(id: string): IActionType<T> {
   };
 }
 
-/**
- * 递归更新节点的位置和大小
- * @param nodes 节点列表
- * @param oldParentRect 父节点的旧尺寸和位置
- * @param newParentRect 父节点的新尺寸和位置
- */
-function recursiveUpdateNodeChildrenSize(nodes: INode[], oldParentRect: IDOMRect, newParentRect: IDOMRect): INode[] {
-  const { width, height } = newParentRect;
-  return nodes.map((node) => {
-    const cxPercent = (node.left + node.width / 2) / oldParentRect.width;
-    const cyPercent = (node.top + node.height / 2) / oldParentRect.height;
-    const widthPercent = node.width / oldParentRect.width;
-    const heightPercent = node.height / oldParentRect.height;
-    const newNodeRect: IDOMRect = {
-      width: widthPercent * width,
-      height: heightPercent * height,
-      left: cxPercent * width - (widthPercent * width) / 2,
-      top: cyPercent * height - (heightPercent * height) / 2,
-    };
-    if (node.children && node.children.length) {
-      return { ...node, ...newNodeRect, children: recursiveUpdateNodeChildrenSize(node.children, { ...node }, { ...newNodeRect }) };
-    } else {
-      return { ...node, ...newNodeRect };
-    }
-  });
-}
-
 // /**
 //  * 递归更新节点的位置和大小
 //  * @param nodes 节点列表
@@ -214,27 +187,16 @@ function recursiveUpdateNodeChildrenSize(nodes: INode[], oldParentRect: IDOMRect
 // function recursiveUpdateNodeChildrenSize(nodes: INode[], oldParentRect: IDOMRect, newParentRect: IDOMRect): INode[] {
 //   const { width, height } = newParentRect;
 //   return nodes.map((node) => {
-//     const nodeAbsolutePosition = CeUtilsService.shared.getAbsolutePosition(
-//       node.left + node.width / 2,
-//       node.top + node.height / 2,
-//       node.width,
-//       node.height,
-//       node.rotate
-//     );
-//     const { tl, tr, bl, br } = CeUtilsService.shared.getItemPercentPositionInGroup(
-//       {
-//         ...oldParentRect,
-//         left: 0,
-//         top: 0,
-//       },
-//       nodeAbsolutePosition
-//     );
-//     const newNodeRect = CeUtilsService.shared.getRelativePosition({
-//       tl: [tl[0] * width, tl[1] * height],
-//       tr: [tr[0] * width, tr[1] * height],
-//       bl: [bl[0] * width, bl[1] * height],
-//       br: [br[0] * width, br[1] * height],
-//     });
+//     const cxPercent = (node.left + node.width / 2) / oldParentRect.width;
+//     const cyPercent = (node.top + node.height / 2) / oldParentRect.height;
+//     const widthPercent = node.width / oldParentRect.width;
+//     const heightPercent = node.height / oldParentRect.height;
+//     const newNodeRect: IDOMRect = {
+//       width: widthPercent * width,
+//       height: heightPercent * height,
+//       left: cxPercent * width - (widthPercent * width) / 2,
+//       top: cyPercent * height - (heightPercent * height) / 2,
+//     };
 //     if (node.children && node.children.length) {
 //       return { ...node, ...newNodeRect, children: recursiveUpdateNodeChildrenSize(node.children, { ...node }, { ...newNodeRect }) };
 //     } else {
@@ -242,3 +204,41 @@ function recursiveUpdateNodeChildrenSize(nodes: INode[], oldParentRect: IDOMRect
 //     }
 //   });
 // }
+
+/**
+ * 递归更新节点的位置和大小
+ * @param nodes 节点列表
+ * @param oldParentRect 父节点的旧尺寸和位置
+ * @param newParentRect 父节点的新尺寸和位置
+ */
+function recursiveUpdateNodeChildrenSize(nodes: INode[], oldParentRect: IDOMRect, newParentRect: IDOMRect): INode[] {
+  const { width, height } = newParentRect;
+  return nodes.map((node) => {
+    const nodeAbsolutePosition = CeUtilsService.shared.getAbsolutePosition(
+      node.left + node.width / 2,
+      node.top + node.height / 2,
+      node.width,
+      node.height,
+      node.rotate
+    );
+    const { tl, tr, bl, br } = CeUtilsService.shared.getItemPercentPositionInGroup(
+      {
+        ...oldParentRect,
+        left: 0,
+        top: 0,
+      },
+      nodeAbsolutePosition
+    );
+    const newNodeRect = CeUtilsService.shared.getRelativePosition({
+      tl: [tl[0] * width, tl[1] * height],
+      tr: [tr[0] * width, tr[1] * height],
+      bl: [bl[0] * width, bl[1] * height],
+      br: [br[0] * width, br[1] * height],
+    });
+    if (node.children && node.children.length) {
+      return { ...node, ...newNodeRect, children: recursiveUpdateNodeChildrenSize(node.children, { ...node }, { ...newNodeRect }) };
+    } else {
+      return { ...node, ...newNodeRect };
+    }
+  });
+}
