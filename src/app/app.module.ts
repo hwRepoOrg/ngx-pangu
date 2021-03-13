45212df4b03be556e6d4bec7c56e7d1ae5453678
MrHuangJser <565628147@qq.com>
2021-03-13 23:56:39 +0800

    MrHuangJser - rename ngx-pangu to ngx-pangu-core

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { CUSTOM_ELEMENTS_SCHEMA, NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { AngularEditorLibModule } from 'ngx-pangu';
+import { AngularEditorLibModule } from '@ngx-pangu/core';
 import { AppComponent } from './app.component';
 
 @NgModule({
